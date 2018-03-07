b02ff3d4c55aff7a93dd22e7a5c982fcd8e9083a
Alexey Minchakov <lexaaim@gmail.com>
2018-03-07 10:31:01 +0300

    + uDataExport * setup default grid's navigator and selection type

---
+++ uDataExport.pas
@@ -0,0 +1,47 @@
+﻿unit uDataExport;
+
+interface
+
+uses cxGrid, cxGridExportLink, ComObj, Excel2000, Windows,
+   Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+   Dialogs, ShellAPI, JPEG, cxExportPivotGridLink, cxCustomPivotGrid, cxPivotGrid,
+  cxPivotGridOLAPDataSource, UnitFileRoutines;
+
+  procedure cxGridToExcelWithImages( const strDocument: string; cxGrid: TcxGrid; AExpand: boolean );
+
+implementation
+
+uses DataModule, UnitExcelHandle;
+
+procedure cxGridToExcelWithImages( const strDocument: string; cxGrid: TcxGrid; AExpand: boolean );
+var
+  ExcelApp, Workbook: Variant;
+  FileName: string;
+  FileTemplate: string;
+  SettingsFile: string;
+begin
+  SettingsFile := GetAppSpecialFolder + 'TempFiles\';
+
+  ExcelApp := CreateOleObject('Excel.Application');
+
+  FileName := SettingsFile + strDocument + '.xlsx';
+  FileTemplate := SettingsFile + strDocument + '.xltx';
+  DeleteFile(FileName);
+  DeleteFile(FileTemplate);
+  FileName := SettingsFile + strDocument + '.xlsx';
+  try
+    ExportGridToXLSX(FileName, cxGrid, AExpand, True, False, 'xlsx');
+  except
+  end;
+  ExcelApp.Application.EnableEvents := false;
+
+  Workbook := ExcelApp.WorkBooks.Add(FileName);
+  Workbook.SaveAs(FileTemplate, 54);
+  Workbook.Close(0);
+  Workbook := ExcelApp.WorkBooks.Add(FileTemplate);
+  ExcelApp.Application.EnableEvents := True;
+
+  ExcelApp.Visible := true;
+end;
+
+end.
--- uOMScxGridDBTableView.pas
+++ uOMScxGridDBTableView.pas
@@ -1,10 +1,18 @@
-unit uOMScxGridDBTableView;
+﻿unit uOMScxGridDBTableView;
 
 interface
 
 uses Classes, cxGridDBTableView, cxGridCustomTableView, cxEdit, cxStyles;
 
 type
+  GridSelectionType = (
+      gstOneCellOneRow,
+      gstAllCellOneRow,
+      gstMultiCellOneRow,
+      gstMultiCellMultiRow,
+      gstAllCellMultiRow
+  );
+
   TOMScxGridDBTableView = class(TcxGridDBTableView)
   private
     procedure InitEditHandler( Sender: TcxCustomGridTableView; AItem: TcxCustomGridTableItem;
@@ -13,18 +21,22 @@ type
     procedure DefaultContentStyle( Sender: TcxCustomGridTableView; ARecord: TcxCustomGridRecord;
       AItem: TcxCustomGridTableItem; var AStyle: TcxStyle );
 
+    procedure NavigatorOnButtonClickHandler( Sender: TObject; AButtonIndex: Integer; var ADone: Boolean );
+
   protected
+    procedure Loaded; override;
+
   published
   public
     constructor Create(AOwner: TComponent); override;
 
-    procedure setSingleCellSelection;
+    procedure setSelectionType( const gst : GridSelectionType );
   end;
 
 implementation
 
 uses uOMSStyle, Windows, cxGraphics, cxDBExtLookupComboBox, cxDBLookupComboBox, cxSpinEdit, cxFilter,
-  cxGridTableView, uDMComponents;
+  cxGridTableView, uDMComponents, cxNavigator, uDataExport, cxGrid;
 
 constructor TOMScxGridDBTableView.Create(AOwner: TComponent);
 begin
@@ -55,20 +67,6 @@ begin
   OptionsView.NoDataToDisplayInfoText := 'НЕТ ДАННЫХ ДЛЯ ОТОБРАЖЕНИЯ';
   OptionsView.GridLineColor := RGB( 190, 190, 190 );
 
-  with NavigatorButtons do begin
-    Insert.Hint := 'Вставить новую запись';
-    Delete.Hint := 'Удалить запись';
-    Post.Hint := 'Сохранить изменения записи';
-    Cancel.Hint := 'Отменить изменения записи';
-
-    Insert.ImageIndex := 0;
-    Delete.ImageIndex := 1;
-    Post.ImageIndex := 2;
-    Cancel.ImageIndex := 3;
-
-    Images := DMOMSComponents.ImageListNavigator;
-  end;
-
   Styles.ContentEven := cxStyleContentDefault;
   Styles.ContentOdd := cxStyleContentOdd;
   Styles.Selection := cxStylePink;
@@ -78,10 +76,59 @@ begin
   OnInitEdit := InitEditHandler;
 end;
 
-procedure TOMScxGridDBTableView.setSingleCellSelection;
+procedure TOMScxGridDBTableView.Loaded;
+var
+  nbtn : TcxNavigatorCustomButton;
 begin
-  OptionsSelection.InvertSelect := False;
-  OptionsSelection.CellSelect := True;
+  inherited;
+
+  setSelectionType(gstAllCellOneRow);
+  Navigator.InfoPanel.DisplayMask := 'Запись: [RecordIndex]/[RecordCount]';
+
+  with NavigatorButtons do
+  begin
+    if (CustomButtons.Count = 0) AND (not Assigned(OnButtonClick)) then
+    begin
+      Images := DMOMSComponents.ImageListNavigator;
+
+      // Tcollection
+      Insert.ImageIndex := 0;
+      Insert.Hint := 'Вставить новую запись';
+      Delete.ImageIndex := 1;
+      Delete.Hint := 'Удалить запись';
+      Post.ImageIndex := 2;
+      Post.Hint := 'Сохранить изменения записи';
+      Cancel.ImageIndex := 3;
+      Cancel.Hint := 'Отменить изменения записи';
+
+      nbtn := CustomButtons.Add;
+      nbtn.ImageIndex := 4;
+      nbtn.Hint := 'Выгрузить в Excel';
+
+      OnButtonClick := NavigatorOnButtonClickHandler;
+    end;
+  end;
+end;
+
+procedure TOMScxGridDBTableView.NavigatorOnButtonClickHandler( Sender: TObject; AButtonIndex: Integer; var ADone: Boolean );
+begin
+  if AButtonIndex >= NBDI_FILTER then
+  begin
+    ADone := True;
+
+    if Navigator.Buttons[ AButtonIndex ].ImageIndex = 4 then  // выгрузка в Excel
+    begin
+      if (Site <> nil) AND (Site.Parent is TcxGrid )
+        then cxGridToExcelWithImages( 'Экранная форма', (Site.Parent as TcxGrid), False );
+    end;
+  end
+  else ADone := False;
+end;
+
+procedure TOMScxGridDBTableView.setSelectionType( const gst : GridSelectionType );
+begin
+  OptionsSelection.InvertSelect := gst in [ gstAllCellOneRow ];
+  OptionsSelection.CellSelect := gst in [ gstAllCellOneRow ];
   OptionsSelection.CellMultiSelect := False;
   OptionsSelection.MultiSelect := False;
 end;
