ccf876dc8d096bbc7dbf4a11bd384456ba4f353f
Alexey Minchakov <lexaaim@gmail.com>
2018-03-08 20:39:36 +0300

    + класс TOMScxLabel

---
+++ uOMScxLabel.pas
@@ -0,0 +1,44 @@
+unit uOMScxLabel;
+
+interface
+
+uses cxLabel, Classes;
+
+type
+  TOMScxLabel = class(TcxLabel)
+  private
+
+  protected
+    procedure Loaded; override;
+
+  published
+  public
+    constructor Create(AOwner: TComponent); override;
+  end;
+
+implementation
+
+uses uOMSStyle, Graphics, Controls;
+
+constructor TOMScxLabel.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+end;
+
+procedure TOMScxLabel.Loaded;
+begin
+  inherited;
+
+  if Assigned(OnDblClick) then
+  begin
+    Style.TextColor := clOMSSpecialBlue;
+    Style.TextStyle := [ fsUnderline ];
+    Cursor := crHandPoint;
+  end;
+
+  if Properties.Alignment.Horz = taLeftJustify
+    then AutoSize := True;
+  Properties.WordWrap := True;
+end;
+
+end.
