bd79b424f51626333de4b377a672c169db159dd7
Alexey Minchakov <lexaaim@gmail.com>
2018-03-08 20:38:35 +0300

    * рефакторинг

--- uOMSStyleGridDefault.pas
+++ uOMSStyleGridDefault.pas
@@ -6,9 +6,9 @@ uses cxStyles, cxGridCustomTableView;
 
 function checkInvalidStyle(var ARecord: TcxCustomGridRecord; var AItem: TcxCustomGridTableItem) : Boolean;
 
-procedure setupStyleGridDefault( var Sender: TcxCustomGridTableView; var ARecord: TcxCustomGridRecord;
+procedure setupStyleGridBefore( var Sender: TcxCustomGridTableView; var ARecord: TcxCustomGridRecord;
       var AItem: TcxCustomGridTableItem; var AStyle: TcxStyle; isHLCursor : Boolean = True );
-procedure setupStyleGridEditable( var Sender: TcxCustomGridTableView; var ARecord: TcxCustomGridRecord;
+procedure setupStyleGridAfter( var Sender: TcxCustomGridTableView; var ARecord: TcxCustomGridRecord;
       var AItem: TcxCustomGridTableItem; var AStyle: TcxStyle );
 
 implementation
@@ -20,7 +20,7 @@ begin
   Result := (ARecord = Nil) OR ( AItem = Nil ) OR (ARecord.Index = -1);
 end;
 
-procedure setupStyleGridDefault( var Sender: TcxCustomGridTableView; var ARecord: TcxCustomGridRecord;
+procedure setupStyleGridBefore( var Sender: TcxCustomGridTableView; var ARecord: TcxCustomGridRecord;
       var AItem: TcxCustomGridTableItem; var AStyle: TcxStyle; isHLCursor : Boolean );
 begin
  if ( isHLCursor ) AND ( Sender.DataController.FocusedRowIndex = ARecord.Index )
@@ -30,7 +30,7 @@ begin
       else AStyle := cxStyleContentOdd;
 end;
 
-procedure setupStyleGridEditable( var Sender: TcxCustomGridTableView; var ARecord: TcxCustomGridRecord;
+procedure setupStyleGridAfter( var Sender: TcxCustomGridTableView; var ARecord: TcxCustomGridRecord;
       var AItem: TcxCustomGridTableItem; var AStyle: TcxStyle );
 var
   AColumn: TcxGridDBColumn;
