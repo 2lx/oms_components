e4c8ef6831c1f683293d55f6be2666ff5714d485
Alexey Minchakov <lexaaim@gmail.com>
2018-03-10 12:24:34 +0300

    * добавлен компонент TOMScxDBLookupComboBox

---
+++ uOMScxDBLookupComboBox.pas
@@ -0,0 +1,83 @@
+unit uOMScxDBLookupComboBox;
+
+interface
+
+uses Classes, cxDBLookupComboBox, Windows;
+
+type
+  TOMScxDBLookupComboBox = class(TcxDBLookupComboBox)
+  private
+    procedure PropertiesChangeHandler(Sender: TObject);
+    procedure DropDownDisableHandler(Sender: TObject);
+    procedure MouseWheelHandler(Sender: TObject; Shift: TShiftState; WheelDelta: Integer;
+          MousePos: TPoint; var Handled: Boolean);
+
+
+  protected
+    procedure Loaded; override;
+    procedure SetEnabled(Value : Boolean); override;
+
+  public
+    constructor Create(AOwner: TComponent); override;
+
+  end;
+
+implementation
+
+uses uOMSStyle, Graphics, cxDropDownEdit;
+
+constructor TOMScxDBLookupComboBox.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+  Style.Color := clOMSEditableHighlight;
+
+  Properties.OnChange := PropertiesChangeHandler;
+  OnMouseWheel := MouseWheelHandler;
+end;
+
+procedure TOMScxDBLookupComboBox.Loaded;
+begin
+  inherited;
+
+  Properties.DropDownListStyle := lsEditFixedList;
+
+  if Properties.DropDownRows < 20
+    then Properties.DropDownRows := 20;
+  if Properties.DropDownWidth < 600
+    then Properties.DropDownWidth := 600;
+end;
+
+procedure TOMScxDBLookupComboBox.MouseWheelHandler(Sender: TObject; Shift: TShiftState;
+  WheelDelta: Integer; MousePos: TPoint; var Handled: Boolean);
+begin
+  Handled := True;
+end;
+
+procedure TOMScxDBLookupComboBox.SetEnabled(Value: Boolean);
+begin
+  inherited SetEnabled(True);
+
+  Properties.ReadOnly := not Value;   // call PropertiesChanged
+  PropertiesChangeHandler(Self);      // баг DevExpress, не всегда вызывается событие PropertiesOnChange
+end;
+
+procedure TOMScxDBLookupComboBox.DropDownDisableHandler(Sender: TObject);
+begin
+  (Sender as TcxCustomDropDownEdit).DroppedDown := False;
+end;
+
+procedure TOMScxDBLookupComboBox.PropertiesChangeHandler(Sender: TObject);
+begin
+  if Properties.ReadOnly
+    then begin
+      Style.Color := clWindow;
+      Properties.OnPopup := DropDownDisableHandler;
+    end
+    else begin
+      Style.Color := clOMSEditableHighlight;
+      Properties.OnPopup := Nil;
+    end;
+end;
+
+end.
