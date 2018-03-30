57da1dd7b598eaa757a9e47ea6545a49232d5397
Alexey Minchakov <lexaaim@gmail.com>
2018-03-30 18:04:02 +0300

    * формат комментария

--- uLogging.pas
+++ uLogging.pas
@@ -3,10 +3,11 @@ unit uLogging;
 interface
 
 procedure logQueryError(const AQName, AQSQLText, QEType, QError : String);
+function formatNoteText( const strText : String ): String;
 
 implementation
 
-uses uDataBase;
+uses uDataBase, SysUtils, uUserSettings;
 
 procedure logQueryError(const AQName, AQSQLText, QEType, QError : String);
 begin
@@ -14,6 +15,11 @@ begin
     DBProcedureNil( 'OMS_PROGRAMERRORLOG_Insert', [ AQName, AQSQLText, QEType, QError ] )
   except
   end;
+end;
+
+function formatNoteText( const strText : String ): String;
+begin
+  Result := #13#10 + FormatDateTime('dd/mm/yy hh:mm', Now ) + ' ' + US.DBUserRusName + ': ' + Trim( strText ) + '.'
 end;
 
 end.
